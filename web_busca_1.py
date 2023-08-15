3363a4c4ab273eaf5d011823aa192a12fdfd003e
P3dro-Vieira <96122489+P3dro-Vieira@users.noreply.github.com>
2023-08-15 18:51:03 -0300

    Subindo o projeto buscar na web

---
+++ web_busca_1.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# ABRINDO O NAVEGADOR
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.google.com.br/')
+time.sleep(1)
+
+# DIGITANDO NO CAMPO DE BUSCA E CLICANDO EM BUSCAR
+driver.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys('silent hill ps1 playthrough')
+time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(Keys.ENTER)
+time.sleep(10)
+driver.quit()
