5f79a14d0c9ecbf98780d98cef3664d1fd1fe890
Pedro Vieira <eng.pedro.vieira@outlook.com>
2025-02-15 12:34:30 -0300

    Atualizando os links

--- web_busca_1.py
+++ web_busca_1.py
@@ -6,12 +6,14 @@ import time
 # ABRINDO O NAVEGADOR
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get('https://www.google.com.br/')
+driver.get('https://www.youtube.com/')
 time.sleep(1)
 
 # DIGITANDO NO CAMPO DE BUSCA E CLICANDO EM BUSCAR
-driver.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys('metal gear 3 remake')
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys(Keys.ENTER)
-time.sleep(10)
-driver.quit()
+driver.find_element(By.XPATH, '//*[@id="center"]/yt-searchbox/div[1]/form/input').send_keys('metal gear 3 remake')
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="center"]/yt-searchbox/div[1]/form/input').send_keys(Keys.ENTER)
+time.sleep(5)
+driver.find_element(By.XPATH, '//*[@id="video-title"]/yt-formatted-string').click()
+time.sleep(85)
+driver.quit() 
